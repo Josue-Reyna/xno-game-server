b6fc45d2c837a23f2cb2dd02774f9c7884df034e
josuerv.8 <josuerv.87@gmail.com>
2023-01-18 16:04:21 -0600

    final fix

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ PORT = process.env.PORT || 3000;
 
 HOST = process.env.HOST || '0.0.0.0';
 
-DB_URI = process.env.DB_URI || "mongodb+srv://jos:pEr6FyTPvIwiPqcG@cluster0.p5j8tre.mongodb.net/?retryWrites=true&w=majority";
+DB_URI = process.env.DB_URI || "mongodb://localhost/test";
 
 module.exports = {PORT, HOST, DB_URI};
